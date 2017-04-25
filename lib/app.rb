a66ce81cc1406ea933ddfac6ba14fbb9ceef333f
JayWebDevCom <jaiye.s@gmail.com>
2017-04-25 12:26:19 +0100

    Introducing params

--- lib/app.rb
+++ lib/app.rb
@@ -8,7 +8,13 @@ get "/secret" do
   'This is a secret page'
 end
 
-get "/cat" do
+get "/random-cat" do
    @cat_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
   erb(:index)
 end
